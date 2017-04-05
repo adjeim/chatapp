9048c6136db3b719ae63de6d4e823c6106da06fb
Mia Adjei <adjei.m@gmail.com>
2017-04-04 22:43:08 -0400

    Add Ionic 2 theme and connect Ionic to app.

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
 import template from './app.html';
 
 @Component({
@@ -6,4 +8,15 @@ import template from './app.html';
 	template
 })
 
-export class MyApp {}
+export class MyApp {
+	constructor(platform: Platform) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      if (platform.is('cordova')) {
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
+		});
+	}
+}
